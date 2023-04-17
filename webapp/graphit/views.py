533132a6737f7d855c4dfed40145cbdd8862b25c
iMaary <m.rodriguesnave@gmail.com>
2023-04-17 10:34:28 -0300

    create playground unique form

--- webapp/graphit/views.py
+++ webapp/graphit/views.py
@@ -11,40 +11,35 @@ def playground(request):
 
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = BasicForm(request.POST)
+        form = GraphForm(request.POST)
 
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
-            edges_form = []
-            n_edges = range(int(form.cleaned_data['n_edges']))
-            for i in n_edges:
-                edge = EdgeForm(request.POST)
-                edges_form.append(edge)
-                
+            n_vertices = form.cleaned_data['n_vertices']
+            n_edges = form.cleaned_data['n_edges']
+            is_directed = form.cleaned_data['is_directed']
+            ids = form.cleaned_data['ids']
+            vertices_1 = form.cleaned_data['vertices_1']
+            vertices_2 = form.cleaned_data['vertices_2']
+            weights = form.cleaned_data['weights']
 
             # redirect to a new URL:
-            return render(request, 'playground/index.html', {'form': form, 'edges_form': edges_form})
+            return render(request, 'playground/index.html', {'form': form})
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = BasicForm()
+        form = GraphForm()
 
     return render(request, 'playground/index.html', {'form': form})
 
 
-class BasicForm(forms.Form):
-    n_vertices = forms.CharField(label='Id do Vertice', max_length=100, widget=forms.TextInput(attrs={'class': 'vertex_area'}))
-    n_edges = forms.CharField(label='Numero de Arestas', max_length=100)
-    is_directed = forms.BooleanField(label='Grafo direcionado (y/n)', required=False)
-
-class EdgeForm(forms.Form):
-    id = forms.CharField(label='Id do Vertice', max_length=100)
-    v1 = forms.CharField(label='Vertice 1', max_length=100)
-    v2 = forms.CharField(label='Vertice 2', max_length=100)
-    weight = forms.CharField(label='Peso', max_length=100)
-
-    def __init__(self, *args, **kwargs):
-        super(EdgeForm, self).__init__(*args, **kwargs)
-        # self.fields['id'].error_messages['required'] = ''
+class GraphForm(forms.Form):
+    is_directed = forms.BooleanField(label='Graph is Directed (y/n)', required=False)
+    n_vertices = forms.CharField(label='Vertices Quantity', max_length=100, widget=forms.TextInput(attrs={'class': 'vertex_area'}))
+    n_edges = forms.CharField(label='Edges Quantity', max_length=100)
+    ids = forms.CharField(label='Ids do Vertice', max_length=100)
+    vertices_1 = forms.CharField(label='Vertices 1', max_length=100)
+    vertices_2 = forms.CharField(label='Vertices 2', max_length=100)
+    weights = forms.CharField(label='Weight (optional)', max_length=100, required=False)
