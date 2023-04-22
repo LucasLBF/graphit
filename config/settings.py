5d3069c4e9ffc4647ca89790cb3f01e4e449937a
LucasLBF <lucas.fernandes1454@gmail.com>
2023-04-22 00:05:25 -0300

    Revert "Add heroku deploy configuration"
    This reverts commit 4a0d6bf430feb601f19cbfc059d4fa2f40f77fe3.

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +133,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-django_heroku.settings(locals())
